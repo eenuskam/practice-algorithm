730e0df567d861ed7a7efeb50be43354a35a7e17
eenuskam <eenuskam@gmail.com>
2025-08-09 23:25:14 +0900

    [Bronze I] Title: 약수들의 합, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.py
@@ -0,0 +1,14 @@
+while True:
+    n = int(input())
+    if n==-1:
+        break
+    
+    total = []
+    for i in range(1,n):
+        if n % i == 0:
+            total.append(i)
+   
+    if sum(total) == n:
+        print(n,' = ',' + '.join(str(i) for i in total),sep="")
+    else:
+        print(f'{n} is NOT perfect.')
