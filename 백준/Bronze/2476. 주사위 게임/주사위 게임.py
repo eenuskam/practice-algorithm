0c591bd170701b364f5655159c66a6feb686a51e
eenuskam <eenuskam@gmail.com>
2025-08-09 11:59:56 +0900

    [Bronze III] Title: 주사위 게임, Time: 60 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2476. 주사위 게임/주사위 게임.py
@@ -0,0 +1,17 @@
+N = int(input())
+rewards = []
+for n in range(N):
+    a,b,c = map(int,input().split())
+    if a==b==c:
+        reward = 10000 + a*1000
+    elif a==b:
+        reward = 1000+a*100
+    elif a==c:
+        reward = 1000+a*100
+    elif b==c:
+        reward = 1000+b*100
+    else:
+        reward = max(a,b,c)*100
+    
+    rewards.append(reward)
+print(max(rewards))
