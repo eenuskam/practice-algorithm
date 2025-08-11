b1e097bcaa43be77d1bd09190b360b410fa916cf
eenuskam <eenuskam@gmail.com>
2025-08-11 12:37:57 +0900

    [Bronze II] Title: 소수 찾기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -0,0 +1,13 @@
+N = int(input())
+nums = list(map(int,input().split())) 
+count = 0
+
+for num in nums:
+    if num == 1:
+        continue
+    for i in range(2,num):
+        if num % i == 0:
+            break
+    else:
+        count+=1
+print(count)
