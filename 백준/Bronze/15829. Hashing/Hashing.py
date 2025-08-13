5361cc3d0671bf72c4050ab1ff500429ebe24d43
eenuskam <eenuskam@gmail.com>
2025-08-13 16:50:37 +0900

    [Bronze II] Title: Hashing, Time: 40 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -1,8 +1,10 @@
+r = 31; M = 1234567891
+
 L = int(input())
 word = list(str(input()))
 score = 0
 
 for i,w in enumerate(word):
-    hash = (ord(w)-96)*(31**i)
+    hash = (ord(w)-96)*(r**i)
     score +=hash
-print(score)
+print(score%M)
