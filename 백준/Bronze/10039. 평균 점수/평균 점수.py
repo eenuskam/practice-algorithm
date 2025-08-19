efa954baea2392bd62af4f34fa4212c2ff05be2d
eenuskam <eenuskam@gmail.com>
2025-08-19 12:42:50 +0900

    [Bronze IV] Title: 평균 점수, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -0,0 +1,11 @@
+total_score = 0
+
+for i in range(5):
+    score = int(input())
+    if score >= 40:
+        score = score
+    else:
+        score = 40
+    
+    total_score+=score
+print(total_score//5) 
